a5a777042d1d3874093cf380ed3629750fb2a798
peachberry <peachberry0318@gmail.com>
2021-05-07 23:29:08 +0900

    refactor(Auth) : signup api 수정

--- src/api/auth/postUserInfo.js
+++ src/api/auth/postUserInfo.js
@@ -1,31 +1,3 @@
-import Axios from 'axios';
-/*
-const postUserInfo = async (email, password, username) => {
-  let user = {
-    id : null,
-    email : "",
-    name : ""
-  };
-
-  await Axios.post(
-    "/auth/signup",
-    {
-      email : email,
-      name : username,
-      password : password
-    }
-  ).then((response) => {
-    user.id = response.data.id;
-    user.email = response.data.email;
-    user.name = response.data.name;
-  }).catch((error) => {
-    console.log(error);
-  });
-
-  return user;
-}
-*/
-
 const postUserInfo = async (user = {}) => {
   let url = '/auth/signup';
 
